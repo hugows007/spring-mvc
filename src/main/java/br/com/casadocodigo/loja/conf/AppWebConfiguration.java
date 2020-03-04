9fa9dab1583ab1a727d315d35f68c5c3d5125cd4
Hugo Bezerra Pimentel <hugobezerrapimentel@gmail.com>
2020-03-04 19:25:59 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -0,0 +1,195 @@
+package br.com.casadocodigo.loja.conf;
+
+import br.com.casadocodigo.loja.controllers.HomeController;
+import br.com.casadocodigo.loja.controllers.ProdutosController;
+import br.com.casadocodigo.loja.dao.ProdutoDAO;
+import br.com.casadocodigo.loja.infra.FileSaver;
+import br.com.casadocodigo.loja.models.CarrinhoCompras;
+import com.google.common.cache.CacheBuilder;
+import com.sun.corba.se.spi.resolver.LocalResolver;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@EnableWebMvc
+@EnableCaching
+@ComponentScan(basePackageClasses = {
+        HomeController.class,
+        ProdutoDAO.class,
+        FileSaver.class,
+        CarrinhoCompras.class
+})
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
+
+    /*
+
+    Configuração das views JSP
+
+     */
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposedContextBeanNames("carrinhoCompras");
+
+        return resolver;
+    }
+
+    /*
+
+    Configuração do arquivo de mensagem de validação do Spring
+
+     */
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("/WEB-INF/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(1);
+
+        return messageSource;
+    }
+
+    /*
+
+    Configuração do formato de data brasileiro
+
+     */
+
+    @Bean
+    public FormattingConversionService mvcConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+        registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
+        registrar.registerFormatters(conversionService);
+
+        return conversionService;
+    }
+
+    /*
+
+    Configuração para upload de arquivos
+
+     */
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    /*
+
+    Configuração do RestTemplate
+
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /*
+
+    Configuração o cache da aplicação - Guava Google
+
+     */
+
+    @Bean
+    public CacheManager cacheManager() {
+        CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .expireAfterAccess(5, TimeUnit.MINUTES);
+
+        GuavaCacheManager manager = new GuavaCacheManager();
+        manager.setCacheBuilder(cacheBuilder);
+
+        return manager;
+    }
+
+    /*
+
+    Configuração do ContentNegotiation - Disponibilizar View e JSON.
+    Para JSON, adicionar um ponto após a requisição - Ex: detalhe/17.json
+        Ou Accept : application/json no Postman, por exemplo.
+
+     */
+    @Bean
+    public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager) {
+        List<ViewResolver> viewResolvers = new ArrayList<>();
+        viewResolvers.add(internalResourceViewResolver());
+        viewResolvers.add(new JsonViewResolver());
+
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setViewResolvers(viewResolvers);
+        resolver.setContentNegotiationManager(manager);
+
+        return resolver;
+    }
+
+	/*
+
+    Configuração que adiciona um locale para alteração do idioma (requisição)
+
+     */
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		return new CookieLocaleResolver();
+	}
+
+	/*
+
+    Configuração que adiciona um Interceptor para alteração do idioma
+
+     */
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
+
+    /*
+
+        Configuração da pasta resources
+
+   */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
